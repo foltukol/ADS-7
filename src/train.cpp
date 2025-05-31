19f47b713d02fa3f8e5f57e7c1627a494f36f683
foltukol <foltukol@yandex.ru>
2025-05-31 17:06:11 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -4,3 +4,18 @@
 #include <ctime>
 
 Train::Train() : countOp(0), first(nullptr) {}
+
+void Train::addCar(bool light) {
+  Car* newCar = new Car{light, nullptr, nullptr};
+  if (!first) {
+    first = newCar;
+    first->next = first;
+    first->prev = first;
+  } else {
+    Car* last = first->prev;
+    last->next = newCar;
+    newCar->prev = last;
+    newCar->next = first;
+    first->prev = newCar;
+  }
+}
